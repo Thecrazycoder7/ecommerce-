478323eb7c756c26f6e898529a84cce7a687b8c3
Thecrazycoder7 <sumankumarib0110@gmail.com>
2024-07-06 17:46:20 +0530

    Layout removed due to no use

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Header from './components/Header';
 import Home from './components/Home';
-import Layout from './components/layout.js';
 function App() {
   return (
     <Router>
